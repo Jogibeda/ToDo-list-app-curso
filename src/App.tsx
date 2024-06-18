92a5d195cd3ff717e5b0bd290471d72517744f82
Jogibeda <117044716+Jogibeda@users.noreply.github.com>
2024-06-18 13:25:08 -0700

    Actualizacion de componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,65 +1,67 @@
-import { ChangeEventHandler, FormEventHandler, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { FormEvent, useEffect, useState } from "react";
 
-const DUMMY_TODOS= [
-  {
-    id:1,
-    title:"Attend the React Course"
-    
-  },
-  {
-    id:2,
-    title:"Build a ToDO List"
-    
-  },
-  {
-    id:3,
-    title:"???"
-    
-  },
-  
-];
+import { Todo, TodoItem } from "./components/todo-item/todo-item";
+import { TodoForm } from "./components/todo-form/todo-form";
+
 function App() {
-  const [todos,setTodos] = useState(DUMMY_TODOS);
-  const [inputValue, setinputValue] = useState("");
-  const handleSubmit: FormEventHandler<HTMLFormElement> = (event) => {
-    event.preventDefault();
-    
-    setTodos((prevtodos) => {
-      const newTodo = {
-        id: Math.floor(Math.random()*1000) + todos.length,
-        title: inputValue,
-      };
-      setinputValue(" ");
-      return [...prevtodos, newTodo]
-      
+  const [todos, setTodos] = useState<Array<Todo>>([]);
 
+  const fetchTodos = async () => {
+    const response = await fetch("https://jsonplaceholder.typicode.com/todos");
+
+    if (!response.ok) {
+      console.error("Failed to fetch todos");
     }
-  )};
-  const handleInputChange: ChangeEventHandler<HTMLInputElement> = (event) => {
-    setinputValue(event.target.value);
-  }
+
+    const data = (await response.json()) as Todo[];
+    const slicedTodos = data.slice(0, 20);
+
+    setTodos(slicedTodos);
+  };
+
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  const handleSubmit = (todoTitle: string) => {
+    setTodos((prevTodos) => {
+      const newTodo: Todo = {
+        id: Math.floor(Math.random() * 1000) + todos.length,
+        title: todoTitle,
+        completed: false,
+        userId: 1,
+      };
+
+      return [newTodo, ...prevTodos];
+    });
+  };
+
+  const handleDelete = (deletedId: number) => {
+    setTodos((previousTodos) => {
+      return previousTodos.filter((todo) => todo.id !== deletedId);
+    });
+  };
+
   return (
-  <main>
-    <h1>ToDO List</h1>
-
-    <form onSubmit={handleSubmit}>
-    <input type="text" value={inputValue} onChange={handleInputChange} />
-    <button>Submit</button>
-    </form>
- 
-    <h2>My ToDOs</h2>
-
-    <ul className="list-inside list-disc">
-      {todos.map((todo) =>(
-        <li key={todo.id}>{todo.id} {todo.title}</li>
-      ))}
-    </ul>
-  </main>
-  
-) ;
-
+    <main className="flex w-full h-screen max-h-screen bg-[#7145d6] flex-col p-2 gap-2">
+      <h1 className="text-2xl font-semibold text-center">ToDo List</h1>
+      <div className="rounded-md bg-violet-400">
+        <div className="flex w-full items-center gap-4 p-2">
+          <h2 className="text-xl font-medium whitespace-nowrap">My ToDos</h2>
+          <TodoForm onSubmit={handleSubmit} />
+        </div>
+        <div className="p-2 bg-violet-300 flex-grow h-full rounded-md">
+          <ul className="flex flex-col gap-2">
+            {todos.map((todo) => {
+              return (
+                <TodoItem key={todo.id} todo={todo} onDelete={handleDelete} />
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+    </main>
+  );
 }
 
-export default App
+export default App;
